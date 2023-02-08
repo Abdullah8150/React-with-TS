f21601961424120403259ff78ff339a31de11ff1
Abdullah8150 <ma8150374@gmail.com>
2023-02-08 18:27:29 +0500

    New Changes

---
+++ src/components/application.tsx
@@ -0,0 +1,10 @@
+// import ControlPanel from './controls';
+import NameBadge from './name-badge';
+
+const Application = () => (
+  <main className="application">
+    <NameBadge name={'Muhammad Abdullah'} />
+  </main>
+);
+
+export default Application;
---
+++ src/components/controls.jsx
@@ -0,0 +1,25 @@
+import PropTypes from 'prop-types';
+
+const ControlPanel = ({ name, onChange }) => {
+  return (
+    <form
+      className="flex flex-row gap-4 text-white bg-primary-10"
+      onSubmit={(event) => event.preventDefault()}
+    >
+      <div>
+        <label className="font-bold">Your Name</label>
+        <input
+          name="name"
+          className="w-full"
+          type="text"
+          value={name}
+          onChange={onChange}
+        />
+      </div>
+    </form>
+  );
+};
+
+
+
+export default ControlPanel;
